2980f9ab97f9b15182c1304627cac84762574c83
Anujaditya02 <anujaditya02@gmail.com>
2020-05-25 12:09:24 +0530

    Updates

---
+++ precache-manifest.bfd437e7bc55b9943911c09d7a0eaf2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37e653c9bc6f0159e187a2e39d0641e",
+    "url": "/Expense-Tracker-React/index.html"
+  },
+  {
+    "revision": "50c18422ad1c0f26bff1",
+    "url": "/Expense-Tracker-React/static/css/main.276928d0.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/Expense-Tracker-React/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Expense-Tracker-React/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c18422ad1c0f26bff1",
+    "url": "/Expense-Tracker-React/static/js/main.9997db66.chunk.js"
+  },
+  {
+    "revision": "9bce5b8ac6b3ff8ef2e8",
+    "url": "/Expense-Tracker-React/static/js/runtime-main.c9dfe57b.js"
+  },
+  {
+    "revision": "3aae104b551ba3970b8dd1a66c790a86",
+    "url": "/Expense-Tracker-React/static/media/expense.3aae104b.jpg"
+  }
+]);
